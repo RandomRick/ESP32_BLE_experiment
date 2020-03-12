c1e8c5ca99261237e472bfb050033ff823176817
Rick Hellicar <rick.hellicar@outlook.com>
2020-03-12 22:27:40 +0000

    fully alinged. Ready for github

--- BLE_notify.ino
+++ BLE_notify.ino
@@ -36,6 +36,7 @@ uint32_t value = 0;
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
+// RJH cribbed from BLE_Write.ino - adding write functionality to this characteristic
 class MyCallbacks: public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharacteristic) {
       std::string value = pCharacteristic->getValue();
